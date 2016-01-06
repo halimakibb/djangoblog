6ed841d2539491af41e2e72fa3f7b687b0c1723a
Habib <flyingwithoutname@gmail.com>
2016-01-06 07:53:43 +0700

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post 
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.order_by('updated')
+    context_dict = {'posts': posts}
+    return render(request, 'blog/index.html', context_dict)
